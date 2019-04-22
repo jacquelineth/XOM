c235b2b55fe534ce358642556259263e02db2501
jacquelineth <jacquelineth@gmail.com>
2019-04-22 11:51:13 +0200

    Visual Code Config file

---
+++ xom/src/test/java/com/ibm/odm/support/xom/DemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ibm.odm.support.xom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
